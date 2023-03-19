7a55a06d076ff9f9ced7097de359b34b0ffdd778
Shaik Aman Sayer <118007035+shaikamansayer13@users.noreply.github.com>
2023-03-19 18:31:20 +0530

    Create overloading_concept.java

---
+++ overloading_concept.java
@@ -0,0 +1,32 @@
+import java.util.*;
+class Student{
+    int id;
+    String name;
+    int marks;
+    //this is simply a method in the class
+    void greet(){
+        System.out.println("Good morning :-" + this.name);
+    }
+    //this is a constructor but here we are passing argumentsin this. 
+    Student(int id,String name,int marks){
+       this.id  = id;
+       this.name = name;
+       this.marks = marks;
+    } 
+    //this is another type of constructor which is having zero constructor.
+    Student(){
+        this.id = 33;
+        this.name = "Kulsum";
+        this.marks = 93;
+    }
+}
+public class Main{
+    public static void main(String [] args){
+        Student aman = new Student(42,"Shaik Aman Sayer",98);
+        Student kulsum = new Student(); 
+        aman.greet();
+        System.out.println(aman.id);
+        System.out.println(aman.name);
+        System.out.println(kulsum.name);
+    }
+}
